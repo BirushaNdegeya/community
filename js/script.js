4a4b907f65886fcd0816de779611520771394cff
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2023-09-12 13:11:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 16:53
+// 17:03 -> create a repository
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    window.alert('Thank you for trying using my web page because I am a beginner who learn how to make responsive design!');
+});
