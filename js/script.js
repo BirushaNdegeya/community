29b5bc7e1a767451e70a0fa3881826c5cacbb8c6
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-29 19:20:48 +0300

    updating component

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// 16:53
-// 17:03 -> create a repository
-
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', () => {
-    window.alert('Thank you for trying using my web page because I am a beginner who learn how to make responsive design!');
-});
